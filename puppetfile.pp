f1e9d2421385ddd0a50cdefd53782d16c3a31682
Dhraj Kalita <dhiraj.sanju@gmail.com>
2017-06-23 16:19:28 +0530

    puppet manifest

---
+++ puppetfile.pp
@@ -0,0 +1,38 @@
+class base {
+	exec { "sudocmd":
+    	path => ["/usr/bin/","/usr/sbin/","/bin"],
+    	command => "yum update -y",
+    	user => root,
+	}
+       
+	package { "binutils-devel":
+        ensure => present,
+    }
+
+	package { "gcc":
+	ensure => present,
+    }
+
+	package {"python-devel":
+	ensure => present,
+    }
+
+	package {"subversion":
+	ensure => present,
+    }
+
+	package {"gcc-gfortran":
+	ensure => present,
+     }
+
+	package {"gcc-c++":
+	ensure => present,
+     }
+
+       service { "condor":
+	ensure => running,
+	enable => true,
+     }
+		
+}
+include base 
